2d42daf7fbc59c9b104306daa17b4c2762718a27
Todd Mostak <tmostak@gmail.com>
2015-03-14 20:46:54 -0700

    Now attenuates delay between animation frames depending on how long querying and rendering take

--- js/crossfilter.mapd.js
+++ js/crossfilter.mapd.js
@@ -157,7 +157,6 @@ function crossfilter() {
     }
 
     function filterDisjunct(disjunctFilters,resetRangeIn) { // applying or with multiple filters"
-      console.log("# Filters: " + disjunctFilters.length);
       var resetRange = false;
       
       if (resetRangeIn != undefined) {
