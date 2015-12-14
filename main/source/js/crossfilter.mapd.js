9deead72b768c8c6e66de1ab5b07155eb28c8acc
Todd Mostak <tmostak@gmail.com>
2015-12-13 16:12:34 -0800

    Change crossfilter so dont use sampling functon if ratio >= 1.0

--- main/source/js/crossfilter.mapd.js
+++ main/source/js/crossfilter.mapd.js
@@ -601,7 +601,7 @@ function crossfilter() {
         query += " WHERE " + filterQuery;
 
       }
-      if (samplingRatio !== null) {
+      if (samplingRatio !== null && samplingRatio < 1.0) {
         if (filterQuery)
           query += " AND ";
         else
