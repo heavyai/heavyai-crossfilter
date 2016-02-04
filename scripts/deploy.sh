351b47f8abe85ed464cbccadc4634281fdbac692
Danny Delott <dannydelott@gmail.com>
2016-02-03 17:17:55 -0800

    Fix deploy scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,38 +3,47 @@
 # show the command being run
 set -x
 
-# tell jenkins to fail if any of the commands fail
+# tell Jenkins to fail if any of the commands fail
 set -e
 
 # If the last commit was from jenkins, do nothing
-if [[ `git log -1 | grep "Jenkins MapD"` ]]; then
- echo "Ignoring superfluous build spawned by jenkins"
- exit 0
+if git log -1 --pretty="%an" | grep -q "Jenkins MapD"; then
+    echo "Ignoring superfluous build spawned by Jenkins"
+    exit 0
 fi
 
+# temporary branch is used since Jenkins checks out detached heads, not named branches
+TMP_BRANCH="temp"
+
 # remove the previous temp branch
-git branch -D temp
+while [[ $(git rev-parse --verify $TMP_BRANCH 2>/dev/null) ]]; do
+    TMP_BRANCH="temp-$RANDOM"
+done
 
 # checkout a new temp branch
-git checkout -b temp
+git checkout -b $TMP_BRANCH
 
 # bump the version
-SEM_VAR=`bash get_latest_semvar_tag.sh | awk 'NR==0; END{print}'`
+SEM_VER=$(bash get_semver_label_from_log.sh | tail -n 1)
+if [[ -z $SEM_VER ]]; then
+    echo "Could not get the latest semver"
+    exit 1;
+fi
 cd ..
-npm --no-git-tag-version version $SEM_VAR
+npm --no-git-tag-version version "$SEM_VER"
 
 # Build the library
 npm install
 node_modules/gulp/bin/gulp.js
 
 # Add and commit the new version
-cd scripts
-git commit -a -m "`bash package_version.sh`"
+pushd scripts
+git commit --all --message="$(bash get_version_number_from_package.sh)"
+popd
 
 # push the new version to github
-git push origin temp:master
+git push origin $TMP_BRANCH:master
 
 # publish the module to npm
-cd ..
 npm publish
 
