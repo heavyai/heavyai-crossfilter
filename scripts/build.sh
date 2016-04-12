996b6735e87ea0d55b02bea3f4271f2cf030146b
Danny Delott <dannydelott@gmail.com>
2016-04-11 17:31:18 -0700

    Add build_output.txt to libaries build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,8 @@
-./node_modules/gulp/bin/gulp.js
+./node_modules/gulp/bin/gulp.js | tee build_output.txt
+grep -q -i 'error' build_output.txt
+if [ "$?" == "0" ]; then
+  exit 1
+fi
+
+rm build_output.txt
+
