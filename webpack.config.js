f7f57a54779fbd71c2987296c8df2457c2a0d6b1
Jonathan Huang <grokut@gmail.com>
2016-06-17 17:35:35 -0700

    [CROSSFILTER] bundle for examples

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var webpack = require("webpack");
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: {
+    "mapd-crossfilter": "./src/mapd-crossfilter.js"
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: "[name].js",
+    libraryTarget: "umd",
+    library: "crossfilter"
+  },
+  devtool: "source-map",
+  externals: {
+    "moment": {
+      "commonjs": "moment",
+      "commonjs2": "moment",
+      "amd": "moment",
+    }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, 'src'),
+        loader: "babel"
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
+  ],
+  resolve: {
+    extensions: ["", ".js"]
+  }
+};
