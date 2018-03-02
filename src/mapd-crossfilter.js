9e8757d1630ca1aec421cd8b4da5383e0e5bb75c
Chris Henrick <chrishenrick@gmail.com>
2018-03-02 14:51:03 -0800

    improved regex for uncast fn, fixes immerse 3825

--- src/mapd-crossfilter.js
+++ src/mapd-crossfilter.js
@@ -162,8 +162,8 @@ function pruneCache(allCacheResults) {
   }, [])
 }
 
-function uncast(string) {
-  const matching = string.match(/^CAST\([a-z,_]{0,250}/)
+function uncast (string) {
+  const matching = string.match(/^CAST\([a-z,A-Z,0-9,_]{0,250}/)
   if (matching) {
     return matching[0].split("CAST(")[1]
   } else {
