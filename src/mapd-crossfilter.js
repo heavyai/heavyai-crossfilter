e621c024fa0f7e2360a9f1bb75fa5b218aab376b
Chris Henrick <chrishenrick@gmail.com>
2018-03-02 15:06:04 -0800

    use shorthand regex in uncast fn

--- src/mapd-crossfilter.js
+++ src/mapd-crossfilter.js
@@ -163,7 +163,7 @@ function pruneCache(allCacheResults) {
 }
 
 function uncast (string) {
-  const matching = string.match(/^CAST\([a-z,A-Z,0-9,_]{0,250}/)
+  const matching = string.match(/^CAST\([\w_]{0,250}/)
   if (matching) {
     return matching[0].split("CAST(")[1]
   } else {
