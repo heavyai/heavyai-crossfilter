f0176b94a25180d4f65bea194657f6e07fc1bb03
Tai Dupree <tdupreetan@gmail.com>
2018-03-05 17:12:50 -0800

    reformat w/ prettier

--- src/modules/group.js
+++ src/modules/group.js
@@ -1,52 +1,52 @@
-import { formGroupSizeQuery } from "./query";
-import { createQueryTask, runQueryTask } from "./task";
+import { formGroupSizeQuery } from "./query"
+import { createQueryTask, runQueryTask } from "./task"
 
 /* istanbul ignore next */
 export function mapResultToArray(queryResult, dimArrayAsArg) {
   return dimArrayAsArg.map(function(v, d) {
-    var varName = "n" + d.toString();
-    return queryResult[varName];
-  });
+    var varName = "n" + d.toString()
+    return queryResult[varName]
+  })
 }
 
 /* istanbul ignore next */
 export function sizeAsyncWithEffects(queryTask, writeFilter) {
   return function sizeAsyncWithState(state, ignoreFilters, callback) {
-    var query = formGroupSizeQuery(writeFilter, state, ignoreFilters);
-    var task = createQueryTask(queryTask, query);
+    var query = formGroupSizeQuery(writeFilter, state, ignoreFilters)
+    var task = createQueryTask(queryTask, query)
     if (!state.multiDim) {
       runQueryTask(task, function(error, result) {
         if (error) {
-          callback(error);
+          callback(error)
         } else {
-          callback(null, result[0].n);
+          callback(null, result[0].n)
         }
-      });
+      })
     } else {
       runQueryTask(task, function(error, result) {
         if (error) {
-          callback(error);
+          callback(error)
         } else {
-          var queryResult = result[0];
-          var multiResult = mapResultToArray(queryResult, state.dimArray);
-          callback(null, multiResult);
+          var queryResult = result[0]
+          var multiResult = mapResultToArray(queryResult, state.dimArray)
+          callback(null, multiResult)
         }
-      });
+      })
     }
-  };
+  }
 }
 
 /* istanbul ignore next */
 export function sizeSyncWithEffects(queryTask, writeFilter) {
   return function sizeSyncWithState(state, ignoreFilters) {
-    var query = formGroupSizeQuery(writeFilter, state, ignoreFilters);
-    var task = createQueryTask(queryTask, query);
+    var query = formGroupSizeQuery(writeFilter, state, ignoreFilters)
+    var task = createQueryTask(queryTask, query)
     if (!state.multiDim) {
-      var result = runQueryTask(task);
-      return result[0].n;
+      var result = runQueryTask(task)
+      return result[0].n
     } else {
-      var queryResult = runQueryTask(task);
-      return mapResultToArray(queryResult, state.dimArray);
+      var queryResult = runQueryTask(task)
+      return mapResultToArray(queryResult, state.dimArray)
     }
-  };
+  }
 }
--- test/task.spec.js
+++ test/task.spec.js
@@ -1,52 +1,52 @@
-import chai, { expect } from "chai";
-import { createQueryTask, runQueryTask } from "../src/modules/task";
-import spies from "chai-spies";
-const noop = () => {};
-chai.use(spies);
+import chai, { expect } from "chai"
+import { createQueryTask, runQueryTask } from "../src/modules/task"
+import spies from "chai-spies"
+const noop = () => {}
+chai.use(spies)
 
 describe("task module", () => {
   describe("createQueryTask function", () => {
-    const query = "test";
-    const options = { queryId: -1 };
-    const method = chai.spy();
-    const thunk = createQueryTask(method, query, options);
-    const callback = noop;
+    const query = "test"
+    const options = { queryId: -1 }
+    const method = chai.spy()
+    const thunk = createQueryTask(method, query, options)
+    const callback = noop
 
     it("should return a function", () => {
-      expect(typeof thunk).to.equal("function");
-    });
+      expect(typeof thunk).to.equal("function")
+    })
 
     it("should run the passed in method with the callback and query", () => {
-      thunk(callback);
-      expect(method).to.have.been.called.with(query, options, callback);
-    });
-  });
+      thunk(callback)
+      expect(method).to.have.been.called.with(query, options, callback)
+    })
+  })
 
   describe("runQueryTask function", () => {
     describe("with callback", () => {
       it("should run task with callback as argument", () => {
-        const task = chai.spy();
-        const callback = noop;
-        runQueryTask(task, callback);
-        expect(task).to.have.been.called.with(callback);
-      });
-    });
+        const task = chai.spy()
+        const callback = noop
+        runQueryTask(task, callback)
+        expect(task).to.have.been.called.with(callback)
+      })
+    })
     describe("without callback", () => {
       it("should run the task and reutrn the result", () => {
-        const result = "test";
-        expect(runQueryTask(() => result)).to.equal(result);
-      });
+        const result = "test"
+        expect(runQueryTask(() => result)).to.equal(result)
+      })
 
       it("should throw an error if the task errors out", () => {
-        const error = "Error";
+        const error = "Error"
         try {
           runQueryTask(() => {
-            throw error;
-          });
+            throw error
+          })
         } catch (e) {
-          expect(e).to.equal(error);
+          expect(e).to.equal(error)
         }
-      });
-    });
-  });
-});
+      })
+    })
+  })
+})
